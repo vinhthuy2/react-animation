b18d48d2086301a6bcad6031f55a6bb839bb85bf
Tran Vinh Thuy <thuy.tranvinh@vn.bosch.com>
2019-05-16 16:32:26 +0700

    show and close modal added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import "./App.css";
-import Modal from "./components/Modal/Modal";
-import Backdrop from "./components/Backdrop/Backdrop";
-import List from "./components/List/List";
+import './App.css';
+import Modal from './components/Modal/Modal';
+import Backdrop from './components/Backdrop/Backdrop';
+import List from './components/List/List';
 
 class App extends Component {
+  state = {
+    modalIsOpen: false
+  };
+
+  showModal = () => {
+    this.setState({ modalIsOpen: true });
+  };
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
+  };
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal />
+        <Modal closed={this.closeModal} show={this.state.modalIsOpen} />
         <Backdrop />
-        <button className="Button">Open Modal</button>
+        <button className="Button" onClick={this.showModal}>
+          Open Modal
+        </button>
         <h3>Animating Lists</h3>
         <List />
       </div>
